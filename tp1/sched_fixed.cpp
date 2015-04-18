b4b1e737287d482122775c1aa58ff71501f045f2
pablo <pablo@pablo.pablo>
2015-04-18 20:22:45 -0300

    sched_fixed incompleto

--- tp1/sched_fixed.cpp
+++ tp1/sched_fixed.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 SchedFixed::SchedFixed(vector<int> argn) {
+	/*SchedFixed recibe la cantidad de cores*/
+	cores = argn[0];
 }
 
 SchedFixed::~SchedFixed() {
@@ -13,11 +15,79 @@ void SchedFixed::initialize() {
 }
 
 void SchedFixed::load(int pid) {
+	/*Encolar una dupla de la forma (periodo(pid),pid)*/
+	q.push(make_pair(period(pid),pid));
 }
 
 void SchedFixed::unblock(int pid) {
+	q.push(make_pair(period(pid),pid));
 }
 
 int SchedFixed::tick(int cpu, const enum Motivo m) {
-	return -2;
+	int sig;
+	if (m == EXIT){
+		/* Si el pid actual termino, buscar proximo*/
+		if (q.empty()){
+			sig = IDLE_TASK;
+		}
+		else{
+			sig = q.top().second; 
+			q.pop();
+		}
+	}
+	else if (m == TICK){
+		/* En TICK, checkear que no haya una más prioritaria lista*/
+		if (q.empty()){
+			/*Si no hay ninguna encolada*/
+			sig = current_pid(cpu);
+		}
+		else{
+			/*Si hay alguna encolada, checkear la prioridad*/
+			if ( (q.top().first) < period(current_pid(cpu))){
+				sig = q.top().second;
+				q.pop();
+			}
+			else{
+				sig = current_pid(cpu);
+			}
+		}
+	}
+	else{
+		/*En BLOCK, desalojar y pasar a la siguiente*/
+		if (q.empty()){
+			sig = IDLE_TASK;
+		}
+		else{
+			sig = q.top().second;
+			q.pop();
+		}
+	}
+	return sig;	
 }
+
+
+/*
+Las lıneas de la forma “&Ar,p,t” crean r tareas de tipo TaskCPU de duracion t y familia
+A cada p unidades de tiempo (es decir la primera en 0p la segunda en 1p la tercera en
+2p y ası hasta completar r tareas).
+*/
+
+/*
+  current pid(cpu): Devuelve el proceso que esta usando el CPU.
+  total_tasks(): Devuelve la cantidad total de tareas que se van a simular (solo para prioridades fijas y dinamicas)
+  period(int pid): Devuelve el perıodo de una tarea.
+  type(int pid): Devuelve la familia de una tarea.
+  declared_cputime(int pid): Devuelve el tiempo de CPU que la tarea va a utilizar.
+*/
+
+/*
+  TICK: la tarea consumio todo el ciclo utilizando el CPU.
+  BLOCK: la tarea ejecuto una llamada bloqueante o permanecio bloqueada durante el ultimo ciclo.
+  EXIT: la tarea termino(ejecuto return).
+*/ 
+
+/*
+  
+
+
+*/ 
--- tp1/sched_fixed.h
+++ tp1/sched_fixed.h
@@ -10,14 +10,23 @@ using namespace std;
 class SchedFixed : public SchedBase {
 	public:
 		SchedFixed(std::vector<int> argn);
-        	~SchedFixed();
+        ~SchedFixed();
 		virtual void initialize();
 		virtual void load(int pid);
 		virtual void unblock(int pid);
 		virtual int tick(int cpu, const enum Motivo m);
 
 	private:
-		std::queue<int> q;
+		/*Cantidad de cores (por si acaso fuera util tenerla)*/
+		int cores;
+		/*Cola de prioridad para las tareas:
+		En la estructura va a haber pares de la forma <periodo(pid),pid>,
+		y la más prioritaria (o sea, la primera que salga con pop() ) 
+		va a ser la que tenga periodo mas pequeño
+		*/
+		priority_queue< pair<int,int>, 
+			vector< pair<int,int> >,
+		   	less< pair<int,int> > > q;
 };
 
 #endif
